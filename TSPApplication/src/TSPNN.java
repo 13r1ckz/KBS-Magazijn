ac34579c844d7c7eee377e586ff09044fba68f1b
Unknown <justinmenges@hotmail.com>
2017-05-10 15:53:16 +0200

    NN algoritme af
    - relatieve locaties ingevoerd ipv absoluut
    - algoritme gaat terug na 3 artikelen
    - aan het eind terug

--- TSPApplication/src/TSPNN.java
+++ TSPApplication/src/TSPNN.java
@@ -1,43 +1,45 @@
 import java.util.ArrayList;
 
 public class TSPNN implements TSPAlgoritme {
-    private int i;
-    private int artikelnr;
-    private int arrayListSize;
     private ArrayList<ArrayList<Integer>> gesorteerd;
     public TSPNN(ArrayList<ArrayList<Integer>> ongesorteerd) {
-        arrayListSize = ongesorteerd.size();
+        int arrayListSize = ongesorteerd.size();
+        int currentX = 0;
+        int currentY = 1;
+        int startX = 0;
+        int startY = 1;
         //bereken verschil tussen de vorige en de huidige afstand in de loop, als verschil kleiner is overschrijft hij de kortste afstand.
         for (int x = 0; x < arrayListSize; x++) {
             int afstand = 100;
             int kortste = 0;
-            //We gaan hier langs al onze producten en onthouden degene met de kortste afstand.
-            int currentX = 1;
-            int currentY = 0;
-            for (i = 0; i < ongesorteerd.size(); i++) {
-
+            for (int i = 0; i < ongesorteerd.size(); i++) {
                 if (afstand > DifCalc(currentX, ongesorteerd.get(i).get(1)) + DifCalc(currentY, ongesorteerd.get(i).get(2))) {
                     afstand = DifCalc(currentX, ongesorteerd.get(i).get(1)) + DifCalc(currentY, ongesorteerd.get(i).get(2));
                     kortste = i;
-                    artikelnr = ongesorteerd.get(i).get(0);
-
+                    int artikelnr = ongesorteerd.get(i).get(0);
                 }
             }
-            //pak x, y en de afstand van het artikel met de korste afstand berekent in bovenstaande loop.
             int kortsteX = ongesorteerd.get(kortste).get(1);
             int kortsteY = ongesorteerd.get(kortste).get(2);
+            //als er 3 producten zijn opgehaald, reset naar start
+            if (x % 3 == 0 && x != 0 ) {
+                System.out.println(x+" item is opgehaald?");
+                System.out.println(startX-currentX);
+                System.out.println(startY-currentY);
+                currentX = startX;
+                currentY = startY;
+            }
             System.out.println("kortste afstand " + afstand + ", x van kortste " + kortsteX + ", y van kortste " + kortsteY );
+            System.out.println(kortsteX - currentX);
+            System.out.println(kortsteY - currentY);
             currentX = kortsteX;
             currentY = kortsteY;
-            System.out.println(currentX);
-            System.out.println(currentY);
-
-
             ongesorteerd.remove(kortste);
-
-
-
         }
+        currentX = startX - currentX;
+        currentY = startY - currentY;
+        System.out.println(currentX);
+        System.out.println(currentY);
     }
     public int DifCalc(int x, int y) {
         if (x != y) {
---
+++ TSPApplication/src/TSPPanel.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Justin on 10/05/2017.
+ */
+public class TSPPanel {
+}
