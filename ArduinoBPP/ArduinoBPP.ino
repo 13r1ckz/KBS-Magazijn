95f30beb726852c960bc2934eeebbabea61f66b0
gerjon <gerjon2@gmail.com>
2017-05-12 14:14:01 +0200

    Comunication
    -Comunucation protocol with code of jasper

--- ArduinoBPP/ArduinoBPP.ino
+++ ArduinoBPP/ArduinoBPP.ino
@@ -1,27 +1,56 @@
-int val = 0 ;
-int led = 13;
-String test = "8.3,3.9,4.1";
+String val = "";
 void setup() {
   Serial.begin(9600);
-  pinMode(led, OUTPUT);
- 
+  for(int x = 0; x < 13; x++){      // port loop //i am lazy
+  pinMode(x, OUTPUT);
+  }
 }
  
 void loop() {
-  delay(100);
+  Serial.println(analogRead(A1));
+  if(val == "E"){             //e shutdonw
+      digitalWrite(4, LOW);
+      digitalWrite(5, 0);
+      Serial.println("Shutdown");
+      val = "";
+    }
+   else if(analogRead(A1) > 200 ){   //top lain
+    digitalWrite(7, HIGH);
+      digitalWrite(6, HIGH);
+    }
+   else if(analogRead(A1) < 200 ){   //top lain
+    digitalWrite(7, LOW);
+      digitalWrite(6, 0);
+    } 
+   else if(analogRead(A0)){   // low lain
+    }
  }
- 
-void serialEvent() {
+  
+void serialEvent() {            // single time updater
   while (Serial.available()) {
-    val = Serial.parseInt();
-    if (val == 1) {
-      digitalWrite(led, HIGH);
+    val = Serial.readString();
+    //Serial.println(val);
+    if (val == 4) {           //Richt action
+      digitalWrite(4, HIGH);
+      digitalWrite(5, 255);
+      delay(1000);
+      Serial.println("R");
+      val = "";
+      digitalWrite(4, LOW);
+      digitalWrite(5, 0);
     }
-    else if (val == 0) {
-      digitalWrite (led, LOW);
+    else if (val == 3) {      //L     action
+      digitalWrite(4, LOW);
+      digitalWrite(5, 255);
+      delay(1000);
+      val = "";
+      Serial.write("R");
+      digitalWrite(4, LOW);
+      digitalWrite(5, 0);
     }
-  }
-  Serial.println("Succes from arduino.");
-  // put your main code here, to run repeatedly:
- 
+    else{                       // notining
+      digitalWrite(4, LOW);
+      digitalWrite(5, 0);
+      }
+  } 
 }
