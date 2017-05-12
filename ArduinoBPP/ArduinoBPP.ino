601dd6783aab84ebae14174bab25552d41cb73da
gerjon <gerjon2@gmail.com>
2017-05-12 16:24:40 +0200

    cobining softwhare finish
    TODO: -single key update
    this wil give insichte and that will make

--- ArduinoBPP/ArduinoBPP.ino
+++ ArduinoBPP/ArduinoBPP.ino
@@ -10,31 +10,12 @@ void setup() {
 }
 
 void loop() {
-  //Serial.println(analogRead(A1));
+  //Serial.println(analogRead(A0));
   if (val == "E") {           //e shutdonw
     digitalWrite(4, LOW);
     digitalWrite(5, 0);
     Serial.println("Shutdown");
     val = "";
-  }
-
-  if (analogRead (A0) <= 350 && side == true) {
-    digitalWrite(4, LOW);
-    analogWrite(5, 255);
-    delay(3000);
-    digitalWrite(4, LOW);
-    analogWrite(5, 0);
-    load = false;
-    side = false;
-  }
-  else if (analogRead (A0) <= 350 && side == false) {
-    digitalWrite(4, HIGH);
-    analogWrite(5, 255);
-    delay(3000);
-    digitalWrite(4, HIGH);
-    analogWrite(5, 0);
-    load = false;
-    side = true;
   }
   if (analogRead (A1) >= 400 && load == false) {
     digitalWrite(7, HIGH);
@@ -50,35 +31,14 @@ void loop() {
     digitalWrite(7, HIGH);
     analogWrite(6, 0);
   }
-
-
 }
 
 void serialEvent() {            // single time updater
   while (Serial.available()) {
-    val = Serial.readString();
-    //Serial.println(val);
-    if (val == 4) {           //Richt action
-      digitalWrite(4, HIGH);
-      digitalWrite(5, 255);
-      delay(1000);
-      Serial.println("R");
-      val = "";
-      digitalWrite(4, LOW);
-      digitalWrite(5, 0);
-    }
-    else if (val == 3) {      //L     action
-      digitalWrite(4, LOW);
-      digitalWrite(5, 255);
-      delay(1000);
-      val = "";
-      Serial.write("R");
-      digitalWrite(4, LOW);
-      digitalWrite(5, 0);
-    }
-    else {                      // notining
-      digitalWrite(4, LOW);
-      digitalWrite(5, 0);
-    }
+    Serial.println("KEY HAS BEEN PRESSED");
+//    else {                      // notining
+//      digitalWrite(4, LOW);
+//      digitalWrite(5, 0);
+//    }
   }
 }
